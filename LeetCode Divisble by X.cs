22c19d7c696e9300863b075e834ae8ffb3e8d691
Sebastianvti <145428545+Sebastianvti@users.noreply.github.com>
2026-08-23 13:48:29 +0200

    LeetCode Tasks

---
+++ LeetCode Divisble by X.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeSeb
+{
+    public class LeetCodeDivisible
+    {
+        private decimal ReduceTotal(decimal Val)
+        {
+            Val--;
+            return Val;
+        }
+        private bool _HasDecimal(decimal Val, int Division) 
+        {  
+            bool isNotWhole =  (Val / Division % 1 != 0);
+            return isNotWhole;
+        }
+
+        public decimal GetDivision(int[] IndexPool, int K)
+        {
+
+            int LoopSum = 0;
+            int LargestNumber = 0;
+            decimal Total = 0;
+            
+
+            foreach (int num in IndexPool)
+            {
+                if (num > LargestNumber) 
+                {
+                    LargestNumber = num;
+                }
+    
+                Total += num;
+            }
+
+            while (_HasDecimal(Total, K))
+            {
+                Console.WriteLine("Looping.");
+                LoopSum++;
+                Total = ReduceTotal(Total);
+            }
+
+
+            return LoopSum;
+        }
+
+    }
+}
---
+++ LeetCode Number of Args Problem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.Intrinsics.Arm;
+using DivisblebyX = LeetCodeSeb.LeetCodeDivisible;
+
+class Program 
+{
+    static void Main() 
+    {
+        Console.WriteLine("hello world");
+        int[] ArrayofInts = { 1, 2, 3, 4, 5};
+        int[] ArrayDivisbleXProblem = { 13, 10, 10 };
+
+        DivisblebyX LeetCodeDivisble = new DivisblebyX();
+        Console.WriteLine(LeetCodeDivisble.GetDivision(ArrayDivisbleXProblem, 5));
+
+
+        CountArgs args = new CountArgs();
+        Console.WriteLine(args.ArgCounter(ArrayofInts));
+    }
+}
+
+public class CountArgs
+{
+    public int ArgCounter(params int[] Parameters)
+    {
+        int count = Parameters.Length;
+        return count;
+    }
+ 
+}
